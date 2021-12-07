925b8997956762160df07b5665395c7865e1b4f4
Seenivasa Ragavan <mailtoragavan.be@gmail.com>
2021-12-07 13:12:17 +1100

    added tasks css

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Task.css';
 
 export default function Task({task: {id, title, state}, onArchiveTask, onPinTask}) {
     return (
